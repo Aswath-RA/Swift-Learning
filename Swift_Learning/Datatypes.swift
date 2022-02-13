91e125baf4212bd01957c17ef1d848277aa61400
Aswath RA <93179993+Aswath-RA@users.noreply.github.com>
2022-02-13 19:16:45 +0530

    Datatype in swift
    Datatype in swift

---
+++ Swift_Learning/Datatypes.swift
@@ -0,0 +1,25 @@
+//Datatypes in swift
+
+
+//Character datatype
+var gender:Character="M"
+print("Your gender is :",gender)
+//String datatype
+var name:String="Aswath"
+print("Your name is :",name)
+
+//Integer datatype
+var age:Int=21
+print("Your age is :",age)
+
+//Float datatype
+var height:Float=157.8
+print("Your height is :",height)
+
+//Double datatype
+var weight:Double=60.78
+print("Your weight is :",weight)
+
+//Boolean datatype
+var marital:Bool=false
+print("Your marital status :",marital)
