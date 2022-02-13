f8cdc3fc3bf67c3ec88846df1066b4cd802ce40c
Aswath RA <93179993+Aswath-RA@users.noreply.github.com>
2022-02-13 19:15:34 +0530

    Variables & Constants in swift
    Variable declaration , initialization and Constants in swift

---
+++ Swift_Learning/Variables.swift
@@ -0,0 +1,29 @@
+//Variables and Constants
+
+
+print("Swift Learning \n")
+//Variable Initialization
+var name = "Aswath"
+print(name)
+
+var height = 157.8
+print(height)
+
+//Variable Declaration
+var number:Int
+number=234
+print(number)
+
+var plang:String
+plang = "swift"
+print(plang)
+
+//Changing value of a variable
+plang="Swift lang" //value of same type can only be changed
+print(plang)
+
+//Constants
+
+let price = 255 //This Price can't be changed after
+print(price)
+
