05b6d20b63f631589546f7c351139eab8647a7a1
Aswath RA <93179993+Aswath-RA@users.noreply.github.com>
2022-02-16 19:36:49 +0530

    Looping statements in Swift

---
+++ Swift_Learning/Looping_Statements.swift
@@ -0,0 +1,28 @@
+//Loops
+
+//For in loop
+print()
+for _ in 1...4 {
+    print("Hello")
+}
+
+//Range in variable
+var x = 5
+var y = 9
+for _ in x...y {
+    print("Hi")
+}
+
+//While loop
+print("While Loop")
+while x > 0 {
+    print(x)
+    x = x-1
+}
+//repeat while loop
+
+repeat {
+    print(y)
+    y = y - 1
+    
+}while y > 0
